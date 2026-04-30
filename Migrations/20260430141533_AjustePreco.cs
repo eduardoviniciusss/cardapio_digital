efdf2fe7266446dcec9957090460a983d05f951d
Eduardo <eduardovjoliveira@gmail.com>
2026-04-30 11:27:53 -0300

    addNovoMigrations

---
+++ Migrations/20260430141533_AjustePreco.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cardapio_digital.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustePreco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Preco",
+                table: "Produtos",
+                type: "numeric(10,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Preco",
+                table: "Produtos",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(10,2)",
+                oldNullable: true);
+        }
+    }
+}
